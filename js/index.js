0e9a98870231e8c1b3d854a09202659f79be3d20
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-21 19:37:47 +0800

    Edit main-screen and story

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-function play() {
-    $("#main-screen").fadeOut(1000);
+function story(mode) {
+    $("#main-screen").fadeOut(500);
+
+    if(mode == 0) { // play as professor
+        $("#story-0").css("visibility", "visible");
+        $("#story-1").css("visibility", "none")
+    } else { // play as students
+        $("#story-0").css("visibility", "none");
+        $("#story-1").css("visibility", "visible");
+    }
+}
+
+function startGame() {
+    $("#story").fadeOut(500);
 }
