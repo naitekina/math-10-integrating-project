25e0675fc0753adbcffc4ad145114b1d7c7ce111
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-21 20:06:05 +0800

    Add positions

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ function story(mode) {
     $("#main-screen").fadeOut(500);
 
     if(mode == 0) { // play as professor
-        $("#story-0").css("visibility", "visible");
-        $("#story-1").css("visibility", "none")
+        $("#story-0").css("display", "visible");
+        $("#story-1").css("display", "none");
     } else { // play as students
-        $("#story-0").css("visibility", "none");
-        $("#story-1").css("visibility", "visible");
+        $("#story-0").css("display", "none");
+        $("#story-1").css("display", "visible");
     }
 }
 
