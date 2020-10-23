4125906918b38fbd95420a3cc22d59b9a96cc652
Emman Evangelista <emmanuel.evangelista@obf.ateneo.edu>
2020-10-24 01:02:52 +0800

    Use canvas instead of containers

---
+++ js/game.js
@@ -0,0 +1,76 @@
+var canvas = null;
+var ctx = null;
+
+var frameDrawer = null;
+
+var numLoaded = 0;
+var maxNumLoaded = 2;
+
+var onLoadedFunction = function() {
+    numLoaded++;
+}
+
+var textures = {
+    battleFoeBox: null,
+    battlePlayerBox: null
+};
+
+function loadTextures() {
+    try {
+        canvas = document.getElementById("game-top");
+        ctx = canvas.getContext("2d");
+
+        textures.battleFoeBox = new Image();
+        textures.battleFoeBox.onload = onLoadedFunction;
+        textures.battleFoeBox.src = "./img/game/battleFoeBoxD.png";
+
+        textures.battlePlayerBox = new Image();
+        textures.battlePlayerBox.onload = onLoadedFunction;
+        textures.battlePlayerBox.src = "./img/game/battlePlayerBoxD.png";
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+
+function drawFrame() {
+    try {
+        if(numLoaded < maxNumLoaded) return;
+
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // clear = fill white
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        drawOpponentInfo();
+        drawPlayerInfo();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+function drawOpponentInfo() {
+    // background sprite
+    ctx.drawImage(textures.battleFoeBox, -32, 32);
+
+    // name
+
+    // Lv.
+
+    // level number
+
+    // hp
+}
+
+function drawPlayerInfo() {
+    // background sprite
+    ctx.drawImage(textures.battlePlayerBox, canvas.width - 260, 192);
+
+    // name
+
+    // Lv.
+
+    // level number
+
+    // hp
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 function story(mode) {
     $("#main-screen").fadeOut(500);
-
-    if(mode == 0) { // play as professor
-        $("#story-0").css("display", "visible");
-        $("#story-1").css("display", "none");
-    } else { // play as students
-        $("#story-0").css("display", "none");
-        $("#story-1").css("display", "visible");
-    }
 }
 
 function startGame() {
     $("#story").fadeOut(500);
+
+    loadTextures();
+
+    frameDrawer = setInterval(drawFrame, 1000 / 30);
 }
+
+function stopGame() {
+    clearInterval(frameDrawer);
+}
+
+// https://www.reddit.com/r/pokemonzetaomicron/comments/1xbnoi/black_white_texture_pack_updated/
